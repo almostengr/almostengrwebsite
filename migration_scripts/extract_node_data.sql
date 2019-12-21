7c1d1ee2c06b24098e07a11c9ef99add774e0113
Almost Engineer <bitsecondal@gmail.com>
2019-12-21 09:04:47 -0600

    Updated migration code

--- migration_scripts/extract_node_data.sql
+++ migration_scripts/extract_node_data.sql
@@ -4,8 +4,6 @@ drop table if exists mkconversion;
 create table mkconversion as 
 select
 node.nid, node.type, node.title, node.status, node.created, node.changed
-, body.body_value, body.body_format
--- , difficulty.field_difficulty_tid
 , (select name from bsal_taxonomy_term_data taxterm where taxterm.tid = difficulty.field_difficulty_tid) as taxterms
 , image.field_image_alt, image.field_image_title, image.field_image_width, image.field_image_height
 , dfile.uri, dfile.filemime
@@ -16,6 +14,36 @@ left join bsal_field_data_field_difficulty difficulty on difficulty.entity_id = 
 left join bsal_field_data_field_image image on image.entity_id = node.nid
 left join bsal_file_managed dfile on dfile.fid = image.field_image_fid
 order by taxterms desc
--- order by field_image_height desc
-
 -- returned 0 rows; check table to confirm data is present
+
+update mkconversion
+set final_output = 
+concat('# ', title, '\n\n', 
+       ifnull(concat('!(images/',replace(replace(uri, 'public://home/bitsecal/ae_article_images/', ''), 'public://sites/default/blog_images/', ''),')\n\n'), ''),
+       ifnull(concat('**Engineer Level: ', taxterms, '** \n\n'), ''),
+       body_value, '\n\n',
+      '**Posted: ', from_unixtime(created), '** \n\n',
+      '**Updated: ', from_unixtime(changed), '** \n\n')
+;
+-- updated 144 rows
+
+update mkconversion
+set final_output =
+replace(replace(replace(replace(final_output, '<p>', ''), '</p>', '\n'), '&nbsp;', ''), '<br />', '')
+;
+
+update mkconversion
+set final_output = replace(replace(replace(replace(final_output, '<code>', '```'), '</code>', '```\n'),'<pre>', '```'), '</pre>', '```\n')
+;
+
+update mkconversion
+set final_output =
+replace(replace(replace(replace(replace(replace(final_output, '<h1>', '#'), '</h1>', ''), '<h2>', '## '), '</h2>', ''), '<h3>', '### '), '</h3>', '')
+;
+
+update mkconversion
+set final_output =
+replace(replace(final_output, '<li>', '* '), '</li>', '')
+;
+-- 15 rows updated
+

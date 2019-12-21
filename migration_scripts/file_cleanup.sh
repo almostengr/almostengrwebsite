fa481812ad09a88fa62d7f095795fb7cc34a9cc1
Almost Engineer <bitsecondal@gmail.com>
2019-12-21 13:50:46 -0600

    Updated individual file cleanup

---
+++ migration_scripts/file_cleanup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for filename in $(ls);
+do
+	sed 's/"#//g' $filename > ${filename}2;
+
+	# sed 's|<br />|\n|g' ${filename}2 > ${filename}3;
+
+	# cp ${filename}3 ${filename}
+
+	# rm *2 *3
+done
+

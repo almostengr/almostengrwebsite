d6ee65b97fdc0b995b0cc2d8c479904e809a36d2
Almost Engineer <bitsecondal@gmail.com>
2020-02-02 22:13:21 -0600

    Updated script; updated navigation

--- scripts/latest_posts.sh
+++ scripts/latest_posts.sh
@@ -9,6 +9,6 @@ echo "Need to be in the docs directory for this script to work"
 echo "# Blog" > blog.md
 echo "" >> blog.md
 
-OUTPUT=$(find ./* -name "*md" -type f -exec ls {} \; | grep -e technology -e lifestyle -e diy -e gardening | sed "s|\./|/|g" | awk -F '/' '{print"* [" $3 " (" $2 ")](" $0 ")"}' | sort -r | grep -v "\[\.")
+OUTPUT=$(find ./* -name "*md" -type f -exec ls {} \; | grep -e technology -e lifestyle -e diy -e gardening | sed "s|\./|/|g" | sed "s|.md||g" | awk -F '/' '{print"* [" $3 " (" $2 ")](" $0 ")"}' | sort -r | grep -v "\[\.")
 
 echo "${OUTPUT}" >> blog.md

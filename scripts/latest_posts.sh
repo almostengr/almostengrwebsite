806fed4384e277bb3df94a4e59f431f35be6a4e2
Almost Engineer <bitsecondal@gmail.com>
2020-02-03 21:54:29 -0600

    #70 Updated to organize blog posts

--- scripts/latest_posts.sh
+++ scripts/latest_posts.sh
@@ -4,11 +4,20 @@
 
 # if [ "${PWD}" ]
 
+OUTPUTFILE="blog/index.md"
+
 echo "Need to be in the docs directory for this script to work"
 
-echo "# Blog" > blog.md
-echo "" >> blog.md
+echo "# Blog" > ${OUTPUTFILE}
+echo "" >> ${OUTPUTFILE}
 
-OUTPUT=$(find ./* -name "*md" -type f -exec ls {} \; | grep -e technology -e lifestyle -e diy -e gardening | sed "s|\./|/|g" | sed "s|.md||g" | awk -F '/' '{print"* [" $3 " (" $2 ")](" $0 ")"}' | sort -r | grep -v "\[\.")
+echo "Not sure what to read or looking for something to read? Then look no further." >> ${OUTPUTFILE}
+echo "There's plenty to read here!" >> ${OUTPUTFILE}
+echo "" >> ${OUTPUTFILE}
 
-echo "${OUTPUT}" >> blog.md
+# OUTPUT=$(find ./* -name "*md" -type f -exec ls {} \; | grep -e technology -e lifestyle -e diy -e gardening | sed "s|\./|/|g" | sed "s|.md||g" | awk -F '/' '{print"* [" $4 " (" $3 ")](" $0 ")"}' | sort -r | grep -v "\[\.")
+
+(grep -r "^# " * | grep -e blog | sed "s|# ||g" | sed "s|blog/blog||g" | awk -F ':' '{print "* [" $2 "](/"$1")"}' | grep -v index.md | sed "s|.md||g" ) >> ${OUTPUTFILE}
+
+# echo "${OUTPUT}" >> ${OUTPUTFILE}
+

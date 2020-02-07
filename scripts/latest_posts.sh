6adeadb302d14432d81b7bea41c2a1da2f0de18b
Almost Engineer <bitsecondal@gmail.com>
2020-02-06 21:34:36 -0600

    Updated script to pull existing page body before adding blog content

--- scripts/latest_posts.sh
+++ scripts/latest_posts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+TMPBLOGFILE="blog/index.tmp"
 BLOGFILE="blog/index.md"
-HEADERTEXTFILE="../scripts/blog_header.md"
 TMPINDEXFILE="index.tmp"
 INDEXFILE="index.md"
 
@@ -9,10 +9,11 @@ echo "INFO: Need to be in the docs directory for this script to work"
 
 BLOGLISTING=$(grep -r -e "^# " * | grep -e blog | grep -v -i -e index.md -e "draft" | sed -e "s|# ||g" -e "s|blog/blog||g" -e "s|:|/|g" -e "s|.md||g" | awk -F '/' '{print $3 "|* ["substr($3,0,10)": "$4" ("$2")](/"$1"/"$2"/"$3")"}' | sort -r | awk -F "|" '{print $2}')
 
-cat ${HEADERTEXTFILE} > ${BLOGFILE}
-echo "${BLOGLISTING}" >> ${BLOGFILE}
+head -n +16 ${BLOGFILE} > ${TMPBLOGFILE}
+echo "${BLOGLISTING}" >> ${TMPBLOGFILE}
+mv ${TMPBLOGFILE} ${BLOGFILE}
 
-head -n -10 ${INDEXFILE} > ${TMPINDEXFILE}
+head -n +20 ${INDEXFILE} > ${TMPINDEXFILE}
 echo "${BLOGLISTING}" | head -10 >> ${TMPINDEXFILE}
 mv ${TMPINDEXFILE} ${INDEXFILE}
 

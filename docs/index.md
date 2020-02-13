73d634417b700e0763f61c265dcf2da579179833
Almost Engineer <bitsecondal@gmail.com>
2020-02-12 19:26:03 -0600

    #71 Added tabs for posts by category

--- docs/index.md
+++ docs/index.md
@@ -27,3 +27,14 @@ his gardening successes and failures and use that as his gardening journal and r
 * [2019.12.29: Greens in Grass (gardening)](/blog/gardening/2019.12.29-greens-in-grass)
 * [2019.12.24: Blog Conversion Steps (technology)](/blog/technology/2019.12.24-blog-conversion-steps)
 * [2019.12.22: You Still Rake Leaves? (gardening)](/blog/gardening/2019.12.22-you-still-rake-leaves)
+* [2020.02.08: Lacing Up My Bootstrap (technology)](/blog/technology/2020.02.08-lacing-up-my-bootstrap)
+ <li><a href="/blog/Blog/"> - </a></li>
+ <li><a href="/blog/technology/2020.02.08-lacing-up-my-bootstrap">2020.02.08 - Lacing Up My Bootstrap</a></li>
+ <li><a href="/blog/technology/2020.02.06-previous-and-current-blog-posting-process">2020.02.06 - Previous and Current Blog Posting Process</a></li>
+ <li><a href="/blog/technology/2020.02.01-developer-uses-page">2020.02.01 - Developer Uses Page</a></li>
+ <li><a href="/blog/gardening/2020.01.25-january-2020-garden-update">2020.01.25 - January 2020 Garden Update</a></li>
+ <li><a href="/blog/technology/2020.01.21-google-search-console-reports-500-errors">2020.01.21 - Google Search Console Reports 500 Errors</a></li>
+ <li><a href="/blog/technology/2020.01.10-7-lessons-from-production-support">2020.01.10 - 7 Lessons Learned as Production Support or Help Desk Agent</a></li>
+ <li><a href="/blog/lifestyle/2020.01.04-21-days-of-prayer-and-fasting">2020.01.04 - 21 Days of Prayer and Fasting </a></li>
+ <li><a href="/blog/lifestyle/2020.01.03-word-of-the-year">2020.01.03 - Word of the Year 2020</a></li>
+ <li><a href="/blog/gardening/2019.12.29-greens-in-grass">2019.12.29 - Greens in Grass</a></li>
--- scripts/latest_posts.sh
+++ scripts/latest_posts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TMPBLOGFILE="blog/index.tmp"
+TMPBLOGFILE="blog.tmp"
 BLOGFILE="blog.md"
 TMPINDEXFILE="index.tmp"
 INDEXFILE="index.md"
@@ -9,13 +9,49 @@ echo "INFO: Need to be in the docs directory for this script to work"
 
 BLOGLISTING=$(grep -r -e "^# " * | grep -e blog | grep -v -i -e index.md -e "draft" | sed -e "s|# ||g" -e "s|blog/blog||g" -e "s|:|/|g" -e "s|.md||g")
 
-BLOGLISTPAGE=$(echo "${BLOGLISTING}" | awk -F '/' '{print $3 "|* ["substr($3,0,11)": "$4" ("$2")](/"$1"/"$2"/"$3")"}' | sort -r | awk -F "|" '{print $2}' | grep -v "(Blog)")
-
-head -n +16 ${BLOGFILE} > ${TMPBLOGFILE}
-echo "${BLOGLISTPAGE}" >> ${TMPBLOGFILE}
-mv ${TMPBLOGFILE} ${BLOGFILE}
+# BLOGLISTPAGE=$(echo "${BLOGLISTING}" | awk -F '/' '{print $3 "|* ["substr($3,0,11)": "$4" ("$2")](/"$1"/"$2"/"$3")"}' | sort -r | awk -F "|" '{print $2}' | grep -v "(Blog)")
+BLOGLISTPAGE=$(echo "${BLOGLISTING}" | awk -F '/' '{print $3 "| <li><a href=\"/"$1"/"$2"/"$3"\">"substr($3,0,11)" - "$4"</a></li>"}' | sort -r | awk -F "|" '{print $2}' | grep -v "(Blog)")
 
 head -n +30 ${INDEXFILE} > ${TMPINDEXFILE}
 echo "${BLOGLISTPAGE}" | head -10 >> ${TMPINDEXFILE}
 
 mv ${TMPINDEXFILE} ${INDEXFILE}
+
+head -n +32 ${BLOGFILE} > ${TMPBLOGFILE}
+# echo "${BLOGLISTPAGE}" >> ${TMPBLOGFILE}
+echo '<div class="tab-content">' >> ${TMPBLOGFILE}
+echo '<div class="tab-pane" id="diy" role="tabpanel" aria-labelledby="diy-tab">' >> ${TMPBLOGFILE}
+echo '<ul>' >> ${TMPBLOGFILE}
+# echo '' >> ${TMPBLOGFILE}
+(echo "${BLOGLISTPAGE}" | grep "diy") >> ${TMPBLOGFILE}
+# echo '' >> ${TMPBLOGFILE}
+echo '</ul>' >> ${TMPBLOGFILE}
+echo '</div>' >> ${TMPBLOGFILE}
+
+echo '<div class="tab-pane" id="gardening" role="tabpanel" aria-labelledby="gardening-tab">' >> ${TMPBLOGFILE}
+echo '<ul>' >> ${TMPBLOGFILE}
+# echo '' >> ${TMPBLOGFILE}
+(echo "${BLOGLISTPAGE}" | grep "gardening") >> ${TMPBLOGFILE}
+# echo '' >> ${TMPBLOGFILE}
+echo '</ul>' >> ${TMPBLOGFILE}
+echo '</div>' >> ${TMPBLOGFILE}
+
+echo '<div class="tab-pane" id="lifestyle" role="tabpanel" aria-labelledby="lifestyle-tab">' >> ${TMPBLOGFILE}
+echo '<ul>' >> ${TMPBLOGFILE}
+# echo '' >> ${TMPBLOGFILE}
+(echo "${BLOGLISTPAGE}" | grep "lifestyle") >> ${TMPBLOGFILE}
+# echo '' >> ${TMPBLOGFILE}
+echo '</ul>' >> ${TMPBLOGFILE}
+echo '</div>' >> ${TMPBLOGFILE}
+
+echo '<div class="tab-pane active" id="technology" role="tabpanel" aria-labelledby="technology-tab">' >> ${TMPBLOGFILE}
+echo '<ul>' >> ${TMPBLOGFILE}
+# echo '' >> ${TMPBLOGFILE}
+(echo "${BLOGLISTPAGE}" | grep "technology") >> ${TMPBLOGFILE}
+# echo '' >> ${TMPBLOGFILE}
+echo '</ul>' >> ${TMPBLOGFILE}
+# echo "${TECHNOLOGYPOSTS}" >> ${TMPBLOGFILE}
+echo '</div>' >> ${TMPBLOGFILE}
+
+echo '</div>' >> ${TMPBLOGFILE}
+mv ${TMPBLOGFILE} ${BLOGFILE}

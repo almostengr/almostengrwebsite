cfc6c61d33ae868ad8fe4e90d7ce28f28d573a8d
Almost Engineer <bitsecondal@gmail.com>
2020-02-12 20:42:24 -0600

    Added comments, updated line count

--- scripts/latest_posts.sh
+++ scripts/latest_posts.sh
@@ -9,13 +9,18 @@ echo "INFO: Need to be in the docs directory for this script to work"
 
 BLOGLISTING=$(grep -r -e "^# " * | grep -e blog | grep -v -i -e index.md -e "draft" | sed -e "s|# ||g" -e "s|blog/blog||g" -e "s|:|/|g" -e "s|.md||g")
 
-# BLOGLISTPAGE=$(echo "${BLOGLISTING}" | awk -F '/' '{print $3 "|* ["substr($3,0,11)": "$4" ("$2")](/"$1"/"$2"/"$3")"}' | sort -r | awk -F "|" '{print $2}' | grep -v "(Blog)")
-BLOGLISTPAGE=$(echo "${BLOGLISTING}" | awk -F '/' '{print $3 "| <li><a href=\"/"$1"/"$2"/"$3"\">"substr($3,0,11)" - "$4"</a></li>"}' | sort -r | awk -F "|" '{print $2}' | grep -v "(Blog)")
+BLOGLISTMD=$(echo "${BLOGLISTING}" | awk -F '/' '{print $3 "|* ["substr($3,0,11)": "$4" ("$2")](/"$1"/"$2"/"$3")"}' | sort -r | awk -F "|" '{print $2}' | grep -v "(Blog)")
 
-head -n +30 ${INDEXFILE} > ${TMPINDEXFILE}
-echo "${BLOGLISTPAGE}" | head -10 >> ${TMPINDEXFILE}
+BLOGLISTPAGE=$(echo "${BLOGLISTING}" | awk -F '/' '{print $3 "| <li><a href=\"/"$1"/"$2"/"$3"\">"substr($3,0,11)" - "$4"</a></li>"}' | sort -r | awk -F "|" '{print $2}' | grep -v "blog/Blog")
+
+##### update index page 
+
+head -n +19 ${INDEXFILE} > ${TMPINDEXFILE}
+echo "${BLOGLISTMD}" | head -10 >> ${TMPINDEXFILE}
 
 mv ${TMPINDEXFILE} ${INDEXFILE}
+
+##### Update blog page
 
 head -n +32 ${BLOGFILE} > ${TMPBLOGFILE}
 # echo "${BLOGLISTPAGE}" >> ${TMPBLOGFILE}

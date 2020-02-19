d620282159b6766448a8557c294a0407e6991c1e
Almost Engineer <bitsecondal@gmail.com>
2020-02-18 19:59:13 -0600

    Updated script to latest index page

--- docs/index.md
+++ docs/index.md
@@ -52,3 +52,16 @@ enjoys fixing things instead of paying someone else to do it for him.
 
 However Kenny has a Business degree from the University of Alabama (Roll Tide!) 
 instead of an engineering degree, thus almost being an engineer.
+
+## Latest Posts
+
+* [2020.02.19 - Mega Zip at Red Mountain Park (lifestyle)](/blog/lifestyle/2020.02.19-mega-zip-at-red-mountain-park)
+* [2020.02.08 - Lacing Up My Bootstrap (technology)](/blog/technology/2020.02.08-lacing-up-my-bootstrap)
+* [2020.02.06 - Previous and Current Blog Posting Process (technology)](/blog/technology/2020.02.06-previous-and-current-blog-posting-process)
+* [2020.02.01 - Developer Uses Page (technology)](/blog/technology/2020.02.01-developer-uses-page)
+* [2020.01.25 - January 2020 Garden Update (gardening)](/blog/gardening/2020.01.25-january-2020-garden-update)
+* [2020.01.21 - Google Search Console Reports 500 Errors (technology)](/blog/technology/2020.01.21-google-search-console-reports-500-errors)
+* [2020.01.10 - 7 Lessons Learned as Production Support or Help Desk Agent (technology)](/blog/technology/2020.01.10-7-lessons-from-production-support)
+* [2020.01.04 - 21 Days of Prayer and Fasting  (lifestyle)](/blog/lifestyle/2020.01.04-21-days-of-prayer-and-fasting)
+* [2020.01.03 - Word of the Year 2020 (lifestyle)](/blog/lifestyle/2020.01.03-word-of-the-year)
+* [2019.12.29 - Greens in Grass (gardening)](/blog/gardening/2019.12.29-greens-in-grass)
--- scripts/latest_posts.sh
+++ scripts/latest_posts.sh
@@ -15,7 +15,7 @@ BLOGLISTPAGE=$(echo "${BLOGLISTING}" | awk -F '/' '{print $3 "| <li><a href=\"/"
 
 ##### update index page 
 
-head -n +19 ${INDEXFILE} > ${TMPINDEXFILE}
+head -n +57 ${INDEXFILE} > ${TMPINDEXFILE}
 echo "${BLOGLISTMD}" | head -10 >> ${TMPINDEXFILE}
 
 mv ${TMPINDEXFILE} ${INDEXFILE}
@@ -23,39 +23,31 @@ mv ${TMPINDEXFILE} ${INDEXFILE}
 ##### Update blog page
 
 head -n +32 ${BLOGFILE} > ${TMPBLOGFILE}
-# echo "${BLOGLISTPAGE}" >> ${TMPBLOGFILE}
+
 echo '<div class="tab-content">' >> ${TMPBLOGFILE}
+
 echo '<div class="tab-pane" id="diy" role="tabpanel" aria-labelledby="diy-tab">' >> ${TMPBLOGFILE}
 echo '<ul>' >> ${TMPBLOGFILE}
-# echo '' >> ${TMPBLOGFILE}
 (echo "${BLOGLISTPAGE}" | grep "diy") >> ${TMPBLOGFILE}
-# echo '' >> ${TMPBLOGFILE}
 echo '</ul>' >> ${TMPBLOGFILE}
 echo '</div>' >> ${TMPBLOGFILE}
 
 echo '<div class="tab-pane" id="gardening" role="tabpanel" aria-labelledby="gardening-tab">' >> ${TMPBLOGFILE}
 echo '<ul>' >> ${TMPBLOGFILE}
-# echo '' >> ${TMPBLOGFILE}
 (echo "${BLOGLISTPAGE}" | grep "gardening") >> ${TMPBLOGFILE}
-# echo '' >> ${TMPBLOGFILE}
 echo '</ul>' >> ${TMPBLOGFILE}
 echo '</div>' >> ${TMPBLOGFILE}
 
 echo '<div class="tab-pane" id="lifestyle" role="tabpanel" aria-labelledby="lifestyle-tab">' >> ${TMPBLOGFILE}
 echo '<ul>' >> ${TMPBLOGFILE}
-# echo '' >> ${TMPBLOGFILE}
 (echo "${BLOGLISTPAGE}" | grep "lifestyle") >> ${TMPBLOGFILE}
-# echo '' >> ${TMPBLOGFILE}
 echo '</ul>' >> ${TMPBLOGFILE}
 echo '</div>' >> ${TMPBLOGFILE}
 
 echo '<div class="tab-pane active" id="technology" role="tabpanel" aria-labelledby="technology-tab">' >> ${TMPBLOGFILE}
 echo '<ul>' >> ${TMPBLOGFILE}
-# echo '' >> ${TMPBLOGFILE}
 (echo "${BLOGLISTPAGE}" | grep "technology") >> ${TMPBLOGFILE}
-# echo '' >> ${TMPBLOGFILE}
 echo '</ul>' >> ${TMPBLOGFILE}
-# echo "${TECHNOLOGYPOSTS}" >> ${TMPBLOGFILE}
 echo '</div>' >> ${TMPBLOGFILE}
 
 echo '</div>' >> ${TMPBLOGFILE}

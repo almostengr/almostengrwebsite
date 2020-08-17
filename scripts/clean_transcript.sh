82d2753c724cfc5e6a5e06b86625dbe33ca3812f
Almost Engineer <bitsecondal@gmail.com>
2020-08-16 19:27:57 -0500

    Rename output file to input file

--- scripts/clean_transcript.sh
+++ scripts/clean_transcript.sh
@@ -1,7 +1,14 @@
 #!/bin/bash 
 
 # Remove lines that begin with 0
-# Also remove lines that are blank
-# ALso convert the text to uppercase
+# remove lines that are blank
+# convert the text to uppercase
 
-sed '/^0/ d' ${1} | sed '/^$/ d' | tr [a-z] [A-Z] > ${1}.out
+INPUTFILE=${1}
+OUTPUTFILE=${INPUTFILE}.out
+
+sed '/^0/ d' ${INPUTFILE} | sed '/^$/ d' | tr [a-z] [A-Z] > ${OUTPUTFILE}
+
+# rename the output file to the input file
+
+mv ${OUTPUTFILE} ${INPUTFILE}

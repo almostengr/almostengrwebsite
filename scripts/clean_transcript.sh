24205909e59af4075ef98233a90f1c38a0b7f7b2
Almost Engineer <bitsecondal@gmail.com>
2020-08-16 19:12:06 -0500

    Updated output file, uppercase the transcript text

--- scripts/clean_transcript.sh
+++ scripts/clean_transcript.sh
@@ -2,5 +2,6 @@
 
 # Remove lines that begin with 0
 # Also remove lines that are blank
+# ALso convert the text to uppercase
 
-sed '/^0/ d' ${1} | sed '/^$/ d'
+sed '/^0/ d' ${1} | sed '/^$/ d' | tr [a-z] [A-Z] > ${1}.out

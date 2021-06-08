cf3fdf89692c3d2ecbe81840b357c5b7ead4dbce
Almost Engineer <bitsecondal@gmail.com>
2021-06-07 22:06:17 -0500

    Removed exclusion, updated autocommit script

--- scripts/ghcommit.sh
+++ scripts/ghcommit.sh
@@ -10,15 +10,15 @@
 STATUSOUTPUT=$(git status)
 
 if [[ ${STATUSOUTPUT} != *"nothing to commit, working tree clean"* ]]; then
-    echo "Videos were found in todays feed"
+    echo "Updates were found"
 
     git config user.name github-actions
     git config user.email github-actions@github.com
     git add .
-    git commit -m "Auto commit latest YouTube video as blog post"
+    git commit -m "Auto commit latest content"
     git push
 
 else
-    echo "No videos were found for today"
+    echo "No updates were found"
     exit 0
 fi

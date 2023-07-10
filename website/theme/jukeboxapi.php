1b44a5e10c0ced3651f99c22816a565e0f0f8342
Almost Engineer <bitsecondal@gmail.com>
2023-07-10 18:41:49 -0500

    Separated endpoints to separate files

--- website/theme/jukeboxapi.php
+++ website/theme/jukeboxapi.php
@@ -75,96 +75,6 @@ abstract class BaseRequestService
     }
 }
 
-final class PutRequestService extends BaseRequestService
-{
-    private string $action;
-    private string $songname;
-
-    public function __construct(string $json)
-    {
-        if (empty($json)) {
-            throw new Exception("Bad request.", 400);
-        }
-
-        $decodedJson = json_decode($json, false);
-        $this->action = $decodedJson->action;
-        $this->songname = $decodedJson->songname ?? "";
-    }
-
-    public function getAction(): string
-    {
-        return $this->action;
-    }
-
-    public function updatePlayingRequestToPlayed(): void
-    {
-        $query = "update songrequest set played = 1 where played = 0";
-        $statement = $this->mysqli->prepare($query);
-        $statement->execute();
-    }
-
-    public function getFirstUnplayedRequest(): array
-    {
-        $query = "SELECT id, sequencename, createdtime FROM songrequest WHERE played = 0 ORDER BY createdTime ASC LIMIT 1";
-        $statement = $this->mysqli->prepare($query);
-
-        if (!$statement->execute()) {
-            throw new Exception("Error getting song.", 500);
-        }
-
-        $result = $statement->get_result();
-        if ($result->num_rows == 0) {
-            $response = new JsonResponse(200, "");
-            $response->toJsonEncode();
-        }
-
-        return $result->fetch_assoc();
-    }
-
-    public function updateCurrentSongPlaying()
-    {
-        $query = "update songsetting set value = ? where identifier = 'currentsong'";
-        $statement = $this->mysqli->prepare($query);
-        $statement->bind_param("s", $this->songname);
-
-        if (!$statement->execute()) {
-            throw new Exception("Error updating song.", 500);
-        }
-
-        $response = new JsonResponse(200, "");
-        $response->toJsonEncode();
-    }
-
-    public function updateRequestToPlayed($row): void
-    {
-        $query = "UPDATE songrequest SET played = 1, modifiedTime = ?, modifiedIpAddress = ? WHERE id = ?";
-        $statement = $this->mysqli->prepare($query);
-
-        $ipAddress = $_SERVER['REMOTE_ADDR'];
-        $statement->bind_param('ssi', date('Y-m-d H:i:s'), $ipAddress, $row['id']);
-        // $statement->bind_param('ssi', $this->currentDateTime, $this->visitorIpAddress, $row['id']);
-        if (!$statement->execute()) {
-            throw new Exception("Error updating song.", 500);
-        }
-
-        $response = new JsonResponse(200, "", $row);
-        $response->toJsonEncode();
-    }
-
-    public function markAllRequestsAsPlayed(): void
-    {
-        $query = "UPDATE songrequest SET played = 1, modifiedTime = ?, modifiedIpAddress = ? where played = 0";
-        $statement = $this->mysqli->prepare($query);
-        $ipAddress = $_SERVER['REMOTE_ADDR'];
-        $statement->bind_param('ss', date('Y-m-d H:i:s'), $ipAddress);
-        if (!$statement->execute()) {
-            throw new Exception("Error updating song.", 500);
-        }
-
-        (new JsonResponse(200, ""))->toJsonEncode();
-    }
-}
-
 final class PostRequestService extends BaseRequestService
 {
     public string $sequenceName;
@@ -283,32 +193,6 @@ $phpInput = "php://input";
 
 try {
     switch ($requestMethod) {
-        case 'PUT':
-            $json = file_get_contents($phpInput);
-            $request = new PutRequestService($json);
-            $request->validateApiKey();
-            $request->connectToDatabase();
-
-            switch ($request->getAction()) {
-                case "nextsong":
-                    // $request->updatePlayingRequestToPlayed();
-                    $song = $request->getFirstUnplayedRequest();
-                    $request->updateRequestToPlayed($song);
-                    break;
-
-                case "clearqueue":
-                    $request->markAllRequestsAsPlayed();
-                    break;
-
-                case "playing":
-                    $request->updateCurrentSongPlaying();
-                    break;
-
-                default:
-                    throw new Exception("Bad request. Invalid action", 400);
-            }
-            break;
-
         case 'GET':
             $request = new GetRequestService();
             $request->connectToDatabase();

5b71afbd2b1ad322cc938aaf9349a29fdc1928c0
almostengr <info@rhtservices.net>
2025-09-21 21:28:48 -0500

    #367 update to have separate rss feeds

--- website/docs/blog/index.md
+++ website/docs/blog/index.md
@@ -6,13 +6,13 @@ category: directorylist
 filtercategory: cooking
 ---
 
-Below are the thoughts that I have on life and a variety of subjects, including tech, 
-home improvement, and more. Being able to write is just as essential as being able 
-to speak, because when those audiences cannot listen to you, they have to be able 
+Below are the thoughts that I have on life and a variety of subjects, including tech,
+home improvement, and more. Being able to write is just as essential as being able
+to speak, because when those audiences cannot listen to you, they have to be able
 to "listen" to your writings.
 
-I will also add, that having a blog, makes it easier to find things, such as checklists, 
-notes from a meeting, and more. This can be done by using the search feature. At the same 
-time, having a blog, means that things that you want to keep up or recall, you can do so 
+I will also add, that having a blog, makes it easier to find things, such as checklists,
+notes from a meeting, and more. This can be done by using the search feature. At the same
+time, having a blog, means that things that you want to keep up or recall, you can do so
 without trying to find the piece of paper that you wrote it down on.
 
--- website/scripts/generate_rss.sh
+++ website/scripts/generate_rss.sh
@@ -1,49 +1,72 @@
 #!/bin/bash
 
-# set -x
+set -x
 
 RSS_FILE="rss.xml"
+RSS_LATEST_FILE="rsslatest.xml"
 
-rm -r $RSS_FILE
+rm -f $RSS_FILE $RSS_LATEST_FILE
 
-touch $RSS_FILE
+touch $RSS_FILE $RSS_LATEST_FILE
 
-echo "<rss version='2.0'>" >> $RSS_FILE
-echo "<channel>" >> $RSS_FILE
-echo "<title>The Blog of Kenny Robinson, The Almost Engineer</title>" >> $RSS_FILE
-echo "<link>http://www.thealmostengineer.com/</link>" >> $RSS_FILE
-echo "<description>Information about this blog and Kenny Robinson.</description>" >> $RSS_FILE
-echo "<language>en-us</language>" >> $RSS_FILE
-echo "<pubDate>$(date)</pubDate>" >> $RSS_FILE
-echo "<lastBuildDate>$(date)</lastBuildDate>" >> $RSS_FILE
-echo "<docs>https://www.rssboard.org/rss-specification</docs>" >> $RSS_FILE
-echo "<!-- <generator>Blosxom 2.1.2</generator>" >> $RSS_FILE
-echo "<managingEditor>neil.armstrong@example.com (Neil Armstrong)</managingEditor>" >> $RSS_FILE
-echo "<webMaster>sally.ride@example.com (Sally Ride)</webMaster> -->" >> $RSS_FILE
-echo "<atom:link href='https://www.rssboard.org/files/sample-rss-2.xml' rel='self' type='application/rss+xml'/>" >> $RSS_FILE
+function writeFeedHeader() {
+    echo "<rss version='2.0'>" >> $1
+    echo "<channel>" >> $1
+    echo "<title>The Blog of Kenny Robinson, The Almost Engineer</title>" >> $1
+    echo "<link>http://www.thealmostengineer.com/</link>" >> $1
+    echo "<description>Information about this blog and Kenny Robinson.</description>" >> $1
+    echo "<language>en-us</language>" >> $1
+    echo "<pubDate>$(date)</pubDate>" >> $1
+    echo "<lastBuildDate>$(date)</lastBuildDate>" >> $1
+    echo "<docs>https://www.rssboard.org/rss-specification</docs>" >> $1
+    echo "<atom:link href='https://www.rssboard.org/files/sample-rss-2.xml' rel='self' type='application/rss+xml'/>" >> $1
+}
+
+function writeFeedFooter() {
+    echo "</channel>" >> $1
+    echo "</rss>" >> $1
+}
+
+function writeFeedItems() {
+    echo "<item>" >> $1
+
+    fileName=$2
+
+    TITLE=$(grep "^title:" "$fileName" | sed 's/^title: //' | xargs)
+    echo "<title>${TITLE}</title>" >> $1
+
+    PAGE_PATH=$(echo $2 | sed "s/docs\/blog//g" | sed "s/.md//g" | xargs)
+    echo "<link>https://thealmostengineer.com/blog${PAGE_PATH}</link>" >> $1
+
+    PUBLISH_DATE=$(grep "^posted:" "$fileName" | sed 's/^posted: //' | xargs)
+    if [ "$PUBLISH_DATE" != "" ]; then
+        echo "<pubDate>${PUBLISH_DATE}</pubDate>" >> $1
+    fi
+
+    DESCRIPTION=$(grep "^description:" "$fileName" | sed 's/^description: //' | xargs)
+    if [ "$DESCRIPTION" != "" ]; then
+        echo "<description>${DESCRIPTION}</description>" >> $1
+    fi
+
+    echo "</item>" >> $1
+}
+
+# write full file
+writeFeedHeader $RSS_FILE
 
 for blogPost in $(ls -r docs/blog/*.md)
 do
-    echo "<item>" >> $RSS_FILE
-
-    TITLE=$(grep -h "title:" "$blogPost" | sed "s/title: //g")
-    echo "<title>${TITLE}</title>" >> $RSS_FILE
-
-    PAGE_PATH=$(echo $blogPost | sed "s/docs\/blog//g" | sed "s/.md//g" )
-    echo "<link>https://thealmostengineer.com/blog${PAGE_PATH}</link>" >> $RSS_FILE
-
-    PUBLISH_DATE=$(grep -h "posted:" "$blogPost" | sed "s/posted: //g")
-    if [ "$PUBLISH_DATE" != "" ]; then
-        echo "<pubDate>${PUBLISH_DATE}</pubDate>" >> $RSS_FILE
-    fi
-
-    DESCRIPTION=$(grep -h "description:" "$blogPost" | sed "s/description: //g")
-    if [ "$DESCRIPTION" != "" ]; then
-        echo "<description>${DESCRIPTION}</description>" >> $RSS_FILE
-    fi
-
-    echo "</item>" >> $RSS_FILE
+    writeFeedItems "$RSS_FILE" "$blogPost"
 done
 
-echo "</channel>" >> $RSS_FILE
-echo "</rss>" >> $RSS_FILE
+writeFeedFooter $RSS_FILE
+
+# write latest file
+writeFeedHeader $RSS_LATEST_FILE
+
+for blogPost in $(ls -r docs/blog/*.md | head -16)
+do
+    writeFeedItems "$RSS_LATEST_FILE" $blogPost
+done
+
+writeFeedFooter $RSS_LATEST_FILE

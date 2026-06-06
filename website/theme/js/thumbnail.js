8de003488c5f581f6aa9b7afa620cbf7cfaee30c
almostengr <info@rhtservices.net>
2026-06-05 21:03:35 -0500

    Updated theme for thumbnails; created post about automated thumbnails

--- website/theme/js/thumbnail.js
+++ website/theme/js/thumbnail.js
@@ -2,7 +2,6 @@ const sideBarColumn = document.getElementById("sidebar");
 const titleColumn = document.getElementById("title");
 
 const params = new URLSearchParams(document.location.search);
-
 const video = params.get("video") ?? null;
 const title = params.get("title") ?? null;
 
@@ -13,7 +12,7 @@ function lightSidebar() {
 
 function darkSidebar() {
     sideBarColumn.classList.add("bg-dark");
-    title.classList.add("text-dark");
+    titleColumn.classList.add("text-dark");
 }
 
 if (title !== null) {
@@ -21,15 +20,9 @@ if (title !== null) {
 }
 
 switch (video) {
-    case "tech":
     case "gardening":
         lightSidebar();
         titleColumn.classList.add("bg-success");
-        break;
-
-    case "personal":
-        darkSidebar();
-        titleColumn.classList.add("bg-info");
         break;
 
     case "handyman":
@@ -37,8 +30,18 @@ switch (video) {
         titleColumn.classList.add("bg-warning");
         break;
 
+    case "personal":
+        darkSidebar();
+        titleColumn.classList.add("bg-info");
+        break;
+
+    case "tech":
+        darkSidebar();
+        titleColumn.classList.add("bg-success-subtle");
+        break;
+
     default:
-        lightSidebar();
-        titleColumn.classList.add("bg-dark");
+        darkSidebar();
+        titleColumn.classList.add("bg-light");
         break;
 }
